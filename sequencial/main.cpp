4e0e89b4eed10f54423cac4a48bada9bc3613da3
stocekdom <dominikstocek2@gmail.com>
2026-02-22 17:52:10 +0100

    Add a PNG library and update main.cpp to create first ray-traced images

--- sequencial/main.cpp
+++ sequencial/main.cpp
@@ -1,6 +1,52 @@
+#include "RayTracer.h"
+#include "lodepng.h"
+#include <memory>
+#include <chrono>
 #include <iostream>
 
 int main()
 {
+   static constexpr int RGBABytes = 4;
 
+   TracerOptions options;
+   options.fieldOfView = 90;
+   options.cameraDistance = 50;
+   options.imageWidth = 1280;
+   options.imageHeight = 720;
+   options.backgroundColor = Color( 10.0f, 10.0f, 10.0f );
+
+   RayTracer rayTracer;
+
+   std::vector<std::shared_ptr<SceneObject>> objects;
+
+   Material orangeMaterial( Color( 220.f, 150.f, 20.f ), Color( 10.f, 5.f, 5.f ), 1.f, 0.5f, 4.f );
+   Material greenMaterial( Color( 20.f, 230.f, 10.f ), Color( 5.f, 10.f, 5.f ), 1.f, 0.5f, 4.f );
+   objects.emplace_back( std::make_shared<Sphere>( Vector3f{ -20.f, 10.f, 100.f }, greenMaterial, 5.f ) );
+   objects.emplace_back( std::make_shared<Sphere>( Vector3f{ 0.f, 0.f, 220.f }, orangeMaterial, 45.f ) );
+
+   auto start = std::chrono::high_resolution_clock::now();
+
+   auto image = RayTracer::generateRawImage( options, objects, {} );
+
+   auto end = std::chrono::high_resolution_clock::now();
+   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( end - start );
+   std::cout << "Image generation took " << duration.count() << " ms" << std::endl;
+   std::cout << "Image is: " << options.imageWidth << "x" << options.imageHeight << " == " << image.size() / RGBABytes <<
+         " pixels and " << image.size() << " bytes." <<
+         std::endl;
+
+   /*
+   // Convert to lodepng format
+   std::vector<unsigned char> image( pixels.size() * RGBABytes );
+
+   for( size_t i = 0; i < pixels.size(); ++i )
+   {
+      image[ i * RGBABytes + 0 ] = pixels[ i ].R;
+      image[ i * RGBABytes + 1 ] = pixels[ i ].G;
+      image[ i * RGBABytes + 2 ] = pixels[ i ].B;
+      image[ i * RGBABytes + 3 ] = pixels[ i ].alpha;
+   }
+*/
+   // Generating png
+   lodepng::encode( "output.png", image, options.imageWidth, options.imageHeight );
 }
