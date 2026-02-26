b4541d2c037971d03a8e26b3f52599b80d609bdf
stocekdom <dominikstocek2@gmail.com>
2026-02-26 22:09:14 +0100

    Add tone mapping, lerp and saturate to math namespacep

--- sequencial/Math.h
+++ sequencial/Math.h
@@ -16,6 +16,31 @@ namespace Math
       return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
    }
 
+   [[nodiscard]] inline float saturate( float value )
+   {
+      return std::clamp( value, 0.0f, 1.0f );
+   }
+
+   [[nodiscard]] inline float reinhardToneMapping( float value )
+   {
+      return value / ( 1.0f + value );
+   };
+
+   [[nodiscard]] inline float acesFilmicToneMapping( float value )
+   {
+      constexpr float a = 2.51f;
+      constexpr float b = 0.03f;
+      constexpr float c = 2.43f;
+      constexpr float d = 0.59f;
+      constexpr float e = 0.14f;
+      return saturate( ( value * ( a * value + b ) ) / ( value * ( c * value + d ) + e ) );
+   }
+
+   [[nodiscard]] inline float lerp( float a, float b, float factor )
+   {
+      return a + factor * ( b - a );
+   }
+
    [[nodiscard]] inline uint8_t uint8ClampMultiplication( uint8_t value, float factor )
    {
       /*
